a7d620e12fbb9c424e071d9162a55cfd1a7c0912
thehaipe <thehaipe2007@gmail.com>
2026-01-30 20:59:29 +0200

    Initial Commit

---
+++ Bufferly/BufferlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BufferlyApp.swift
+//  Bufferly
+//
+//  Created by Valentyn on 30.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct BufferlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
